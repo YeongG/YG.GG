4f82f22aba6b300ae649af280248b78555fcbabc
YeongG <yeonggil223@gmail.com>
2020-07-23 16:16:13 +0900

    file -> V3 dir

--- src/Modules/index.js
+++
@@ -1,8 +0,0 @@
-import searchPlayerName from './searchPlayerName';
-import { combineReducers } from 'redux';
-
-const rootReducer = combineReducers({
-    searchPlayerName
-});
-
-export default rootReducer;
